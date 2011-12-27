9bb49e525e634e79bf10fe9d1a54e3e8cfa6a5c8
Ruben d'Arco <cyclops@prof-x.net>
2011-12-28 00:27:32 +0100

    - Moved 'next:' label to prevent crash when goto next was called and d_values had a 0 count. - Added timestamp support; still needs work as sometimes no items are returned.

--- modules/tinydnsbackend/tinydnsbackend.cc
+++ modules/tinydnsbackend/tinydnsbackend.cc
@@ -1,38 +1,61 @@
 #include "tinydnsbackend.hh"
+#include <cdb.h>
 #include <pdns/dnslabel.hh>
 #include <pdns/misc.hh>
 #include <pdns/dnsrecords.hh>
 #include <boost/foreach.hpp>
 
+const string backendname="[TinyDNSBackend]";
 vector<string> CDB::findall(const string &key)
 {
 	vector<string> ret;
 	struct cdb cdb;
 	struct cdb_find cdbf; /* structure to hold current find position */
 
-	int	fd = open(d_cdbfile.c_str(), O_RDONLY);
+	int fd = open(d_cdbfile.c_str(), O_RDONLY);
+	if (fd < 0)
+	{
+		L<<Logger::Error<<backendname<<" Failed to open cdb database file '"<<d_cdbfile<<"'. Error: "<<stringerror()<<endl;
+		throw new AhuException(backendname + " Failed to open cdb database file '"+d_cdbfile+"'. Error: " + stringerror());
+	}
 
-	cdb_init(&cdb, fd);
-	fprintf(stderr, "key [%s] length [%d]\n", key.c_str(), key.size());
-	fprintf(stderr, "cdb_findinit: %d\n", cdb_findinit(&cdbf, &cdb, key.c_str(), key.size()));
-	cerr<<"doing cdb lookup of key ["<<makeHexDump(key)<<"]"<<endl;
+	int cdbinit = cdb_init(&cdb, fd);
+	if (cdbinit < 0) 
+	{
+		L<<Logger::Error<<backendname<<" Failed to initialize cdb database. ErrorNr: '"<<cdbinit<<endl;
+		throw new AhuException(backendname + " Failed to initialize cdb database.");
+	}
+	fprintf(stderr, "[findall] key [%s] length [%lu]\n", key.c_str(), key.size());
+	cerr<<"[findall] doing cdb lookup of key ["<<makeHexDump(key)<<"]"<<endl;
+
+	cdb_findinit(&cdbf, &cdb, key.c_str(), key.size());
+	int x=0;
 	while(cdb_findnext(&cdbf) > 0) {
+		x++;
 		unsigned int vpos = cdb_datapos(&cdb);
 		unsigned int vlen = cdb_datalen(&cdb);
 		char *val = (char *)malloc(vlen);
 		cdb_read(&cdb, val, vlen, vpos);
 		string sval(val, vlen);
-		ret.push_back(sval);
 		// cerr<<"got value ["<<makeHexDump(sval)<<"]"<<endl;
+		ret.push_back(sval);
 		free(val);
 	}
+	fprintf(stderr, "[findall] Found [%d] records for key [%s]\n", x, key.c_str());
 	close(fd);
 	return ret;
 }
 
-TinyDNSBackend::TinyDNSBackend()
+
+
+
+TinyDNSBackend::TinyDNSBackend(const string &suffix)
 {
-	d_cdb=new CDB("/tmp/data.cdb");
+	setArgPrefix("tinydns"+suffix);
+	d_cdb=new CDB(getArg("dbfile"));
+	
+	//TODO: Make constant or define? 
+	d_taiepock = 4611686018427387904ULL;
 }
 
 bool TinyDNSBackend::list(const string &target, int domain_id)
@@ -44,10 +67,9 @@ void TinyDNSBackend::lookup(const QType &qtype, const string &qdomain, DNSPacket
 {
 	DNSLabel l(qdomain.c_str());
 	string key=l.binary();
-	vector<string> res;
 
-	L<<Logger::Debug<<"query for qtype "<<qtype.getName()<<" qdomain ["<<qdomain<<"]"<<endl;
-	L<<Logger::Debug<<"key ["<<makeHexDump(key)<<"]"<<endl;
+	L<<Logger::Debug<<"[lookup] query for qtype ["<<qtype.getName()<<"] qdomain ["<<qdomain<<"]"<<endl;
+	L<<Logger::Debug<<"[lookup] key ["<<makeHexDump(key)<<"]"<<endl;
 	d_qtype=qtype;
 	d_values=d_cdb->findall(key);
 	d_qdomain = qdomain;
@@ -55,13 +77,17 @@ void TinyDNSBackend::lookup(const QType &qtype, const string &qdomain, DNSPacket
 
 bool TinyDNSBackend::get(DNSResourceRecord &rr)
 {
-	if(!d_values.size())
-	{
-		return false;
-	}
-	else
-	{
-		next:
+	L<<Logger::Debug<<"[GET]"<<endl;
+
+	//TODO: Labels aren't very nice. Maybe find another way of doing this?
+	next:
+		if(!d_values.size())
+		{
+			cerr<<"Returning false..."<<endl;
+			return false;
+		}
+		else
+		{
 			string val = d_values.back();
 			d_values.pop_back();
 			QType valtype;
@@ -72,13 +98,18 @@ bool TinyDNSBackend::get(DNSResourceRecord &rr)
 			copy(sval, sval+len, bytes.begin());
 			PacketReader pr(bytes);
 			// rec=(struct tinyrecord *) val.c_str();
-			cerr<<"in get: got value ["<<makeHexDump(val)<<"]"<<endl;
+			cerr<<"in get: got value ["<<makeHexDump(val)<<"]" <<endl;
 			valtype = QType(pr.get16BitInt());
 			cerr<<"value has qtype "<<valtype.getName()<<endl;
 			cerr<<"query has qtype "<<d_qtype.getName()<<endl;
 			char locwild = pr.get8BitInt();
-			if(locwild != '\075') // TODO: wildcards; TODO: locations
+			if(locwild != '\075') 
+			{
+			 	// TODO: wildcards; 
+				// TODO: locations
+				cerr<<"wildcard char, or location"<<endl;
 				goto next;
+			}
 			if(d_qtype.getCode()==QType::ANY || valtype==d_qtype)
 			{
 				cerr<<"WE GOT HIM"<<endl;
@@ -87,9 +118,27 @@ bool TinyDNSBackend::get(DNSResourceRecord &rr)
 				rr.qname = d_qdomain;
 				rr.ttl = pr.get32BitInt();
 
-				uint64_t timestamp = (pr.get32BitInt() << 32) + pr.get32BitInt(); // TODO: this is actually broken
-				if(timestamp) // TODO: support timestamps?
-					goto next;
+
+				uint64_t timestamp = pr.get32BitInt();
+				timestamp <<= 32;
+				timestamp += pr.get32BitInt();
+				if(timestamp) 
+				{
+					uint64_t now = d_taiepock + time(NULL);
+					cerr<<"TIMESTAMP:"<<timestamp<<endl;
+					cerr<<"      NOW:"<<now<<endl;
+					uint32_t diff = timestamp - now;
+					cerr<<"     DIFF:"<< diff<<endl;
+					if (timestamp < now)
+					{
+						cerr<<"Record is old, do not return."<<endl;
+						goto next;
+					}
+					if (rr.ttl == 0)
+					{
+						rr.ttl = timestamp - now;
+					}
+				}
 
 				cerr<<"passing to mastermake ["<<makeHexDump(sval)<<"]"<<endl;
 
@@ -100,21 +149,22 @@ bool TinyDNSBackend::get(DNSResourceRecord &rr)
 				DNSRecordContent *drc = DNSRecordContent::mastermake(dr, pr);
 
 				string content = drc->getZoneRepresentation();
-	            if(rr.qtype.getCode() == QType::MX || rr.qtype.getCode() == QType::SRV)
-	            {
+				if(rr.qtype.getCode() == QType::MX || rr.qtype.getCode() == QType::SRV)
+				{
 					vector<string>parts;
 					stringtok(parts,content," ");
 					rr.priority=atoi(parts[0].c_str());
 					rr.content=content.substr(parts[0].size()+1);
-	            }
-	            else
-	            {
+				}
+				else
+				{
 					rr.content = content;
-	            }
+				}
 				cerr<<"rr.priority: "<<rr.priority<<", rr.content: ["<<rr.content<<"]"<<endl;
 			}
+			cerr<<"Returning true..."<<endl;
 			return true;
-	}
+		}
 }
 
 // boilerplate
@@ -123,9 +173,15 @@ class TinyDNSFactory: public BackendFactory
 public:
 	TinyDNSFactory() : BackendFactory("tinydns") {}
 
-	DNSBackend *make(const string &suffix)
+	void declareArguments(const string &suffix="")
 	{
-		return new TinyDNSBackend();
+		declare(suffix, "dbfile", "Location of the cdb data file", "data.cdb");
+	}
+
+
+	DNSBackend *make(const string &suffix="")
+	{
+		return new TinyDNSBackend(suffix);
 	}
 };
 
@@ -136,7 +192,6 @@ public:
 	TinyDNSLoader()
 	{
 		BackendMakers().report(new TinyDNSFactory);
-
 		L<<Logger::Info<<" [TinyDNSBackend] This is the TinyDNSBackend ("__DATE__", "__TIME__") reporting"<<endl;
 	}
 };
--- modules/tinydnsbackend/tinydnsbackend.hh
+++ modules/tinydnsbackend/tinydnsbackend.hh
@@ -23,19 +23,22 @@ private:
 class TinyDNSBackend : public DNSBackend
 {
 public:
-	TinyDNSBackend();
+	TinyDNSBackend(const string &suffix);
 	// ~TinyDNSBackend();
 	void lookup(const QType &qtype, const string &qdomain, DNSPacket *pkt_p=0, int zoneId=-1);
 	bool list(const string &target, int domain_id);
 	bool get(DNSResourceRecord &rr);
 	// bool getSOA(const string &name, SOAData &soadata, DNSPacket *p=0);
 private:
+	uint64_t d_taiepock;
 	int d_fd;
 	QType d_qtype;
 	CDB *d_cdb;
 	vector<string> d_values;
 	string d_qdomain;
 };
+
+
 
 struct tinyrecord
 {
